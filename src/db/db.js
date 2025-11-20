763e75bb8565f8a16deac97b11d66730276d9205
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-11-20 14:14:12 +0530

    db conneted successfully

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,12 @@
-import mongoose, { connect } from "mongoose";
-import { DB_NAME } from "../constant.js";
+import mongoose, { connect } from "mongoose"
+import { DB_NAME } from "../constant.js"
 
 const connectDatabase = async () => {
     try {
         const connects = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected :: DB_HOST: ${connects.connection.host}`)
     } catch (error) {
-        console.error("Mongo_db_connection error:", error);
+        console.error("Mongo_db_connection error:", error)
         process.exit(1)
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@ console.log("GoFlow...........")
 
 import express from 'express'
 import dotenv from "dotenv"
- dotenv.config({
+dotenv.config({
     path: './.env'
 })
+import connectDatabase from './db/db.js'
 const app = express()
 
-const port = process.env.port || 6001
 
 
-app.get('/api/v1', (_, res) => {
+connectDatabase()
+    .then(() => {
+        console.log("Database connected")
+        app.listen(process.env.PORT || 6001, () => {
+            console.log(`server is ready at ${process.env.PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.error("Database connection failed", error)
+    })
+
+app.get('/api/v1', (req, res) => {
     res.send('Welcome to the Goflow App')
 })
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`server is ready at ${process.env.PORT}`);
-})
