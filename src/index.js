069160aa01c68f79e3017877efb9b65db16ff69e
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-11-20 14:06:09 +0530

    added db connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log("GoFlow...........")
+
+import express from 'express'
+import dotenv from "dotenv"
+ dotenv.config({
+    path: './.env'
+})
+const app = express()
+
+const port = process.env.port || 6001
+
+
+app.get('/api/v1', (_, res) => {
+    res.send('Welcome to the Goflow App')
+})
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`server is ready at ${process.env.PORT}`);
+})
