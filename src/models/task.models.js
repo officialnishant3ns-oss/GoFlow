181cbff6f2ee6b7813bc6a838ff008a7078315b2
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-11-22 18:09:45 +0530

    added  task models

---
+++ src/models/task.models.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Task title is required"],
+            trim: true,
+        },
+
+        description: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+
+        status: {
+            type: String,
+            enum: ["pending", "in-progress", "completed"],
+            default: "pending",
+        },
+
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            default: "medium",
+        },
+
+        dueDate: {
+            type: Date,
+            default: null,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        subtasks: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Subtask",
+            }
+        ],
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema)
+export default Task
