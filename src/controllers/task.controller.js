5e389125fdbe9d89739effe02c4acbc8070a2333
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-11-26 19:25:39 +0530

    added task create portion

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,4 +1,23 @@
 import Task from "../models/task.models"
 import Subtask from "../models/subtask.models"
+import asynchandler from "../utils/asynchandler"
 
 
+const CreateTask = asynchandler(async (req,res) => {
+    const{title,description,priority,dueDate,subtasks} = req.body
+    if (!title) {
+        throw new apierror(400, "Title is required");
+    }
+    const newtask = await Task.create({
+         description: description || "",
+        priority: priority || "medium",
+        dueDate: dueDate ? new Date(dueDate) : null,
+        owner: req.user._id,              
+        subtasks: Array.isArray(subtasks) ? subtasks : [] 
+    })
+    
+     return res.status(201).json(
+        new apiresponse(200, newtask, "Task created successfully")
+    )
+})
+export {CreateTask}
